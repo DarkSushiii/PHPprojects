eef7c430d022e67fdd76dd400482306a4b271dc0
M303user <m303user@M303.uc-bcf.edu.ph>
2023-10-03 18:28:55 +0800

    Initial Commit

---
+++ 04_variable_types.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Variable Types</title>
+</head>
+<body>
+<?php 
+
+$int_var = 12345;
+$another_int = 12345 + 12345;
+
+echo $another_int;
+?>
+</body>
+</html>
