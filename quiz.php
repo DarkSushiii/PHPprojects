678ff50491ef24a9b67b749ba8b312676751a345
Darwin <dbt1748@students.uc-bcf.edu.ph>
2023-10-12 19:06:42 +0800

    commit quiz

---
+++ quiz.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Variable Types</title>
+</head>
+<body>
+<?php 
+    echo "tite";
+    echo "123";
+    echo "puke";
+    echo "etits";
+?>
+</body>
+</html>
